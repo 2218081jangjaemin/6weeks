dbcf8707e74a3aa374210d7f58ba0fdf6520e598
2218081jangjaemin <144421522+2218081jangjaemin@users.noreply.github.com>
2023-10-06 16:49:00 +0900

    6주차 퀴즈입니다.
    퀴즈 제출합니다.

--- 6주차 퀴즈 2218081 장재민.py
+++ 6주차 퀴즈 2218081 장재민.py
@@ -33,13 +33,3 @@ for name in pingpong_list:
 
 for mail in results:
     print(mail)
-
-def print_gugudan(n):
-    for x in range(1, n+1):
-        print(f"------[ {x} 단 ] ------")
-        for y in range(1, n+1):
-            print(f"{x} x {y} = {x*y}")
-
-num = input("몇 단까지 출력할까요? ")
-num = int(num)
-print_gugudan(num)
